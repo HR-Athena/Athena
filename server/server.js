3b5e1f514b579cd33a868179ab7272793271fab9
Andrey Azov <andrey.azov@gmail.com>
2015-08-06 03:56:59 +0300

    Added favicon

--- server/server.js
+++ server/server.js
@@ -5,5 +5,6 @@ var favicon = require('serve-favicon');
 var app = express();
 
 app.use(express.static(__dirname + "/../client"));
+app.use(favicon(__dirname + '/../client/favicon.ico'));
 
 module.exports = app;
