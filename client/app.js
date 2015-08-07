35dc2106f4d4ddce97881d9dc7e3ba0a30a0f579
Chris Griffis <christophergriffis@gmail.com>
2015-08-07 14:19:35 -0400

    Client Side Framework

---
+++ client/app.js
@@ -0,0 +1,11 @@
+/*Entry Point for routing*/
+var congressmanprofile = angular.module('congressmanprofile', [
+  // add in controller dep
+  //ex congressmanprofile.home
+  'ui-router'
+  ])
+
+// Router for app
+.config() {
+
+}
