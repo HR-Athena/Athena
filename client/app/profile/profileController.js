519ea95aa152ef482718d4f15d33ab4900000543
Syed Jafri <syedjafri@gmail.com>
2015-08-11 21:53:38 -0400

    Updated Profile Page

--- client/app/profile/profileController.js
+++ client/app/profile/profileController.js
@@ -5,14 +5,12 @@ module.exports = function profileController($scope, $stateParams, Home){
   console.log('I am profile controller');
   
   var selectedPerson = {};
+  
 
   $scope.memberId=$stateParams.id;
-  $scope.billDetails = {};
-
-
 
   $scope.allMembers = {}; // app.allMembers; // TODO: Check if OK
-  $scope.member = {}; // app.member; // TODO: Check if OK
+  $scope.member = {}; 
   
   getMember($scope.memberId);
   getMemberVotes($scope.memberId);
@@ -36,6 +34,7 @@ module.exports = function profileController($scope, $stateParams, Home){
     Home.getMember(id)
     .then(function(data){
       $scope.member = data;
+      $scope.member.age=calculateAge(new Date($scope.member.birthday));
 
     }).catch(function(err){
       throw err;
@@ -51,39 +50,17 @@ module.exports = function profileController($scope, $stateParams, Home){
     Home.getMemberVotes(id)
     .then(function(data){
       $scope.member.votes = data;
-       console.log($scope.member);
-      getBillDetails(3);
     }).catch(function(err){
       throw err;
     });
   }
 
-  function getBillDetails(max){
-    var count = 0;
-    var voteIds = Object.keys($scope.member.votes);
-    
-    // voteIds.forEach(function (id){
 
-    // });
-
-    for (var bill_id in $scope.member.votes){
-      //console.log("bill_id:"+bill_id);
-      if (count < max){
-        Home.getBillDetails(bill_id) 
-        .then(function(data){
-          // console.log("******", data);
-          // console.log("again_bill_id:"+bill_id);
-          if (!(bill_id in $scope.billDetails)){
-            $scope.billDetails[bill_id] = data;
-            //console.log("=====", $scope.billDetails);
-          }
-        }).catch(function(err){
-          throw err;
-        });
-        count++;
-      }
-    }
+  function calculateAge(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
   }
-
 };
+
 
