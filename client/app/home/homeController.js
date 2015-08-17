f5328d8456c9f7bdd81d8bb35a511dc1a12761be
Andrey Azov <andrey.azov@gmail.com>
2015-08-18 02:15:39 +0300

    Working scope

--- client/app/home/homeController.js
+++ client/app/home/homeController.js
@@ -21,19 +21,20 @@ module.exports = function homeController($scope, $state, Home){
   $scope.getStateMembers = function(state) {
     $scope.stateMembers = [];
 
-    for (var i=0; i<$scope.allMembers.length; i++){
+    for (var i = 0; i < $scope.allMembers.length; i++){
       var memberTitle = $scope.allMembers[i].title;
       if(memberTitle.match(/.*\[.*\-(.{2})/)){
         var memberState = memberTitle.match(/.*\[.*\-(.{2})/)[1];
         if (memberState === state){
-          $scope.stateMembers.push($scope.allMembers[i]);
+           $scope.stateMembers.push($scope.allMembers[i]);
         }
       } else {
         console.log("did not work ", memberTitle);
       }
-
     }
-    console.log($scope.stateMembers);
+    $scope.$apply(function(){
+      $scope.stateMembers = $scope.stateMembers;
+    });
 
   };
 
