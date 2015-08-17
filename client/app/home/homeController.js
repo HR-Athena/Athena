bda7f2337d490078e6bd593c82b47264fb4d501b
Cliff Saporta Cheng <ccfcheng@gmail.com>
2015-08-17 16:29:25 -0700

    More styling changes

--- client/app/home/homeController.js
+++ client/app/home/homeController.js
@@ -7,10 +7,15 @@ module.exports = function homeController($scope, $state, Home){
   $scope.member = {};
   $scope.allMembers = Home.allMembers;
   $scope.trendingMembers = Home.trendingMembers;
+  $scope.isMapView = false;
 
   $scope.gotoMember = function(){
     var id = $scope.memberSearch.id;
     $state.go('profile', {id:id});
+  };
+
+  $scope.switchView = function() {
+    $scope.isMapView = $scope.isMapView ? false : true;
   };
 
 };
