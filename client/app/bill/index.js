a0cf93a4b4cd0595fa238ff246a1037583783329
Eric King <e.king117@gmail.com>
2015-08-20 22:34:45 -0400

    Fixed bug that overwrote variable from another controller.

--- client/app/bill/index.js
+++ client/app/bill/index.js
@@ -3,6 +3,6 @@ var angular = require("angular");
 var billController = require("./billController.js");
 var Home = require('../home/homeFactory.js');
 
-module.exports = angular.module("congressmanprofile.profile", [])
+module.exports = angular.module("congressmanprofile.bill", [])
   .controller('billController', billController)
   .factory('Home', Home);
